97147eb75f60fdb663181e16aae4b0e59a1937c2
Aleksey <opelat.a@gmail.com>
2021-03-31 00:06:35 +0300

    sphere and walls working correct

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using static UnityEngine.Input;
+
+
+public class Controller : MonoBehaviour
+{
+    [SerializeField] private float _speed = 10f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        ControlLMove();
+    }
+
+    private void ControlLMove()
+    {
+        if (GetKey(KeyCode.W))
+        {
+            Movement(Vector3.forward);
+        }
+        else if (GetKey(KeyCode.S))
+        {
+            Movement(Vector3.back);
+        }
+        else if (GetKey(KeyCode.A))
+        {
+            Movement(Vector3.left);
+        }
+        else if (GetKey(KeyCode.D))
+        {
+            Movement(Vector3.right);
+        }
+    }
+    private void Movement(Vector3 direction)
+    {
+        transform.position += direction * _speed * Time.deltaTime;
+    }
+}
